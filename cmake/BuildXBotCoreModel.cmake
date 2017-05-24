0b8b34057dcd455dd96083e6d822b7ae5263be08
Luca Muratore <luca.muratore@iit.it>
2017-05-24 12:31:42 +0200

    Fix BuildXBotCoreModel cmake files with master TAG

--- cmake/BuildXBotCoreModel.cmake
+++ cmake/BuildXBotCoreModel.cmake
@@ -9,7 +9,7 @@ ycm_ep_helper(  XBotCoreModel
                 TYPE GIT
                 STYLE GITHUB
                 REPOSITORY ADVRHumanoids/xbotcoremodel.git
-                TAG centauro_bonn_review_2016
+                TAG master
                 COMPONENT external
                 DEPENDS srdfdom_advr
 			XBotCore-interfaces
