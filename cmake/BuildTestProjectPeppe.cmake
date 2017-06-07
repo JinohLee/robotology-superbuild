c4dfa548516c8d467a14e577baae1b3c590ca05a
pepperig <giusepperig@hotmail.it>
2017-06-07 18:12:13 +0200

    Add new project

---
+++ cmake/BuildTestProjectPeppe.cmake
@@ -0,0 +1,14 @@
+# comanapp
+#include(YCMEPHelper)
+#include(FindOrBuildPackage)
+
+#find_or_build_package(YARP QUIET)
+
+#ycm_ep_helper(  comanapp
+#                TYPE GIT
+#                STYLE GITHUB
+#                REPOSITORY ADVRHumanoids/comanapp.git
+#                TAG master
+#                COMPONENT robots
+#                DEPENDS YARP
+ #           )
