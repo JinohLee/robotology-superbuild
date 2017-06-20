9a87ae87df57b341f9665897c8f3186ccc9240f5
Luca Muratore <luca.muratore@iit.it>
2017-06-20 10:43:34 +0200

    ADVR_shared is on GITHUB

--- cmake/BuildADVR_shared.cmake
+++ cmake/BuildADVR_shared.cmake
@@ -4,8 +4,8 @@ include(FindOrBuildPackage)
 
 ycm_ep_helper(  ADVR_shared
                 TYPE GIT
-                STYLE GITLAB_ADVRCLOUD
-                REPOSITORY advr_humanoids/advr_shared.git
+                STYLE GITHUB
+                REPOSITORY advr_humanoids/ADVR_shared.git
                 TAG master
                 COMPONENT configs
 		BUILD_COMMAND ""
