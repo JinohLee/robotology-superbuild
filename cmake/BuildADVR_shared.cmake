322666c13d7e9e75eb9538e5e3e489bf0d19a95a
Rajesh Subburaman <rajesh.subburaman@iit.it>
2017-06-20 10:49:25 +0200

    ADVRHumanoids group name for github

--- cmake/BuildADVR_shared.cmake
+++ cmake/BuildADVR_shared.cmake
@@ -5,7 +5,7 @@ include(FindOrBuildPackage)
 ycm_ep_helper(  ADVR_shared
                 TYPE GIT
                 STYLE GITHUB
-                REPOSITORY advr_humanoids/ADVR_shared.git
+                REPOSITORY ADVRHumanoids/ADVR_shared.git
                 TAG master
                 COMPONENT configs
 		BUILD_COMMAND ""
